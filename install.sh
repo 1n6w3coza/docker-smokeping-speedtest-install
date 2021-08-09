8af0c2eade28238f0b3fa75aac48b9f9f88b4c2b
1n6w3coza <62834566+1n6w3coza@users.noreply.github.com>
2021-08-09 11:25:51 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 mkdir /opt/smokeping-speedtest
 docker run -d --name=smokeping-speedtest -e PUID=1000 -e PGID=1000 -e TZ=Africa/Johannesburg -p 80:80 -v /opt/smokeping-speedtest/config:/config -v /opt/smokeping-speedtest/data:/data --restart unless-stopped jwigley/smokeping-speedtest
+#download Target & Probe
+#restart container
